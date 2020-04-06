400915a8d68fb0de7d6c711f27a0deba7056a880
ana Dias <analuisa104@gmail.com>
2020-04-06 18:00:46 -0300

    feat: crete basic structure for questions and answers

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,30 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answerQuestion(){
+    print("a resposta foi escolhida!");
+
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text("OIIII"));
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Quiz"),
+        ),
+        body: Column(
+          children: <Widget>[
+            Text("Pergunta!"),
+            RaisedButton(child: Text("Resposta"), onPressed: answerQuestion,),
+            RaisedButton(child: Text("Resposta"), onPressed: answerQuestion),
+            RaisedButton(child: Text("Resposta"), onPressed: answerQuestion),
+            RaisedButton(child: Text("Resposta"), onPressed: answerQuestion)
+          ],
+        ),
+      ),
+    );
   }
 }
